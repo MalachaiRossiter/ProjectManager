7795babcd25aa377d983495980c7a9f55d3b3d8f
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-11 23:39:48 -0500

    set up backend server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
+require('./routes/product.routes')(app); //importing the exported routes so that we can use them in the server
 
 app.listen(8000, () => console.log(`Listening on Port: 8000`));
